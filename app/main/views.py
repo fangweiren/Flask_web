1d1e96fe9a47ca4e88e29b7b9d2bc27e1a508114
FANG <513069595@qq.com>
2017-08-18 12:30:34 +0800

    优化评论排序显示前端及其它

--- app/main/views.py
+++ app/main/views.py
@@ -119,23 +119,45 @@ def post(id):
 		pagination=pagination)
 
 
-@main.route('/dianzan/<int:id>', methods=['GET', 'POST'])
+@main.route('/sort_by_time/<int:id>', methods=['GET', 'POST'])
 @login_required
-def sort_by_likes(id):
+def sort_by_time(id):
 	post = Post.query.get_or_404(id)
 	form = CommentForm()
 	if form.validate_on_submit():
 		comment = Comment(body=form.body.data, post=post, author=current_user._get_current_object())
 		db.session.add(comment)
 		flash('您的评论已发布.')
-		return redirect(url_for('.post', id=post.id, page=-1))
+		return redirect(url_for('.sort_by_time', id=post.id, page=-1))
+	mark = False
+	page = request.args.get('page', 1, type=int)
+	if page == -1:
+		page = (post.comments.count() - 1) // current_app.config['FLASKY_COMMENTS_PER_PAGE'] + 1
+	pagination = post.comments.order_by(Comment.timestamp.asc()).paginate(
+		page, per_page=current_app.config['FLASKY_COMMENTS_PER_PAGE'], error_out=False)
+	comments = pagination.items
+	return render_template('post.html', posts=[post], form=form, comments=comments, mark=mark,
+		pagination=pagination)
+
+
+@main.route('/sort_by_likes/<int:id>', methods=['GET', 'POST'])
+@login_required
+def sort_by_likes(id):
+	post = Post.query.get_or_404(id)
+	form = CommentForm()
+	if form.validate_on_submit():
+		comment = Comment(body=form.body.data, post=post, author=current_user._get_current_object    ())
+		db.session.add(comment)
+		flash('您的评论已发布.')
+		return redirect(url_for('.sort_by_likes', id=post.id, page=-1))
+	mark = True
 	page = request.args.get('page', 1, type=int)
 	if page == -1:
 		page = (post.comments.count() - 1) // current_app.config['FLASKY_COMMENTS_PER_PAGE'] + 1
 	pagination = post.comments.order_by(Comment.likes.desc()).paginate(
 		page, per_page=current_app.config['FLASKY_COMMENTS_PER_PAGE'], error_out=False)
 	comments = pagination.items
-	return render_template('post.html', posts=[post], form=form, comments=comments,
+	return render_template('post.html', posts=[post], form=form, comments=comments, mark=mark,
 		pagination=pagination)
 
 
